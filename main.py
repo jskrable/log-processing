724ffdcdd45329a280997608922d635db130ca6e
Jack Skrable <jskrable@bu.edu>
2019-07-24 10:34:46 -0400

    added arg parser

--- main.py
+++ main.py
@@ -8,11 +8,33 @@ jack skrable
 
 import json
 import collections
+import sys
+import string
+import argparse
 
-log_file = './data/20190624230312.json'
 
-with open(log_file, encoding='utf8') as f:
-    data = json.load(f)
+# Progress bar for cli
+def progress(count, total, suffix=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+    percents = round(100.0 * count / float(total), 1)
+    bar = '#' * filled_len + '-' * (bar_len - filled_len)
+    sys.stdout.write('[%s] %s%s %s\r' % (bar, percents, '%', suffix))
+    sys.stdout.flush()
+
+
+def arg_parser():
+    # function to parse arguments sent to CLI
+    # setup argument parsing with description and -h method
+    parser = argparse.ArgumentParser(
+        description='Summarizes logs from a json file')
+    parser.add_argument('-f', '--file', default='./datasets/sample.json', type=str, nargs='?',
+                        help='path of the file to summarize')
+    # parse args and return
+    args = parser.parse_args()
+    return args
+
+
 
 
 def dig_logs(in_rec, errors=[], path=None):
@@ -30,6 +52,20 @@ def dig_logs(in_rec, errors=[], path=None):
         [dig_logs(x, errors, path) for x in in_rec]
     return errors
 
+
+# MAIN
+#####################################################################
+args = arg_parser()
+INFILE = args.file
+
+# log_file = './data/load_errors.json'
+
+with open(INFILE, encoding='utf8') as f:
+    data = json.load(f)
+
 errors = dig_logs(data)
-summary = collections.Counter(errors)
+summary = dict(collections.Counter(errors))
+
+print('Error Message    :   Count')
+print(summary)
 
